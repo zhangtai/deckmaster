3b234b3b7ad8a342cf117af539f1bc07f8917d7e
Tai Zhang <tai.zhang@outlook.com>
2022-11-26 08:19:32 +0800

    Add redis widget

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/bendahl/uinput v1.6.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/flopp/go-findfont v0.1.0
+	github.com/go-redis/redis/v9 v9.0.0-rc.1 // indirect
 	github.com/godbus/dbus v4.1.0+incompatible
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240
@@ -20,7 +21,6 @@ require (
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible
-	github.com/stretchr/testify v1.2.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.9 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
---
+++ widget_redis.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"log"
+	"strings"
+	"time"
+	"context"
+	"github.com/go-redis/redis/v9"
+)
+
+var ctx = context.Background()
+
+// CommandWidget is a widget displaying the output of command(s).
+type RedisWidget struct {
+	*BaseWidget
+
+	keys     []string
+	fonts    []string
+	frames   []image.Rectangle
+	colors   []color.Color
+}
+
+// NewRedisWidget returns a new RedisWidget.
+func NewRedisWidget(bw *BaseWidget, opts WidgetConfig) *RedisWidget {
+	bw.setInterval(time.Duration(opts.Interval)*time.Millisecond, time.Second)
+
+	var keys, fonts, frameReps []string
+	_ = ConfigValue(opts.Config["keys"], &keys)
+	_ = ConfigValue(opts.Config["font"], &fonts)
+	_ = ConfigValue(opts.Config["layout"], &frameReps)
+	var colors []color.Color
+	_ = ConfigValue(opts.Config["color"], &colors)
+
+	layout := NewLayout(int(bw.dev.Pixels))
+	frames := layout.FormatLayout(frameReps, len(keys))
+
+	for i := 0; i < len(keys); i++ {
+		if len(fonts) < i+1 {
+			fonts = append(fonts, "regular")
+		}
+		if len(colors) < i+1 {
+			colors = append(colors, DefaultColor)
+		}
+	}
+
+	return &RedisWidget{
+		BaseWidget: bw,
+		keys:       keys,
+		fonts:      fonts,
+		frames:     frames,
+		colors:     colors,
+	}
+}
+
+// Update renders the widget.
+func (w *RedisWidget) Update() error {
+	size := int(w.dev.Pixels)
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
+
+	for i := 0; i < len(w.keys); i++ {
+		str, err := getValue(w.keys[i])
+		if err != nil {
+			return err
+		}
+		font := fontByName(w.fonts[i])
+
+		drawString(img,
+			w.frames[i],
+			font,
+			str,
+			w.dev.DPI,
+			-1,
+			w.colors[i],
+			image.Pt(-1, -1))
+	}
+	return w.render(w.dev, img)
+}
+
+func getValue(key string) (string, error) {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379",
+        Password: "",
+        DB:       0,
+    })
+    output, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		return strings.TrimSuffix(string("nil"), "\n"), nil
+	}
+	log.Println("key", output)
+	return strings.TrimSuffix(string(output), "\n"), nil
+}
